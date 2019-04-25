4059c02eb925a7861e551e0e4d75c1b8a858bfa4
AdalbertoMaranhao <limadalberto@gmail.com>
2019-04-25 18:26:35 -0300

    função de captura implementada.

--- app/src/main/java/com/example/e_checklist/MainActivity.kt
+++ app/src/main/java/com/example/e_checklist/MainActivity.kt
@@ -1,26 +1,73 @@
 package com.example.e_checklist
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.support.v4.content.FileProvider
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    var currentPath: String? = null
+    val TAKE_PICTURE = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_preencher.setOnClickListener {
-            val intent = Intent(this, GabaritoActivity::class.java)
-
-            startActivity(intent)
+        btn_capturar.setOnClickListener {
+            dispatchCameraIntent()
         }
         btn_gabarito.setOnClickListener{
             val pdf_url = "http://oab.fgv.br/GABARITOS_RETIFICADOS_XXVIII_EXAME_DE_ORDEM.pdf"
             val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(pdf_url))
             startActivity(browserIntent)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == TAKE_PICTURE && resultCode == Activity.RESULT_OK){
+            try {
+                val file = File(currentPath)
+                val uri = Uri.fromFile(file)
+                imageView.setImageURI(uri)
+            }catch (e: IOException){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun dispatchCameraIntent(){
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if(intent.resolveActivity(packageManager) != null){
+            var photoFile: File? = null
+            try {
+                photoFile = createImage()
+            }catch (e: IOException){
+                e.printStackTrace()
+            }
+            if (photoFile != null){
+                startActivityForResult(intent, TAKE_PICTURE)
+            }
+        }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    fun createImage(): File{
+        val timestamp = SimpleDateFormat("yyyyMMdd_hhmmss").format(Date())
+        val imageName = "JPG_"+timestamp+"_"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(imageName, ".jpg", storageDir)
+        currentPath = image.absolutePath
+        return image
     }
 }
